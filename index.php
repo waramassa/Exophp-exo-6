084e9d9badcfe76292d35ed8aba1ba750de675a2
Wara Massa <waramassa70@gmail.com>
2020-11-24 22:41:41 +0100

    php exo6

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$number = 140;
+?>
+<!DOCTYPE html>
+<html lang="efr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP exo6</title>
+</head>
+<body>
+    <p><?php echo ($number + 30 ) / 2;?></p>
+</body>
+</html>
